c7c2bb54ce7f262e323826e40b43503578309a69
grantkee <grant.d.kee@gmail.com>
2019-08-27 23:19:29 -0500

    FINAL ready to turn in

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,15 @@
 const split = (str, delim) => {
-  // write code for strings.split
-
+  return str.split(delim)
 }
 
 const pairs = (str) => {
-  // write code for strings.pairs
-
+  return str.match(/(..?)/g)
 }
 
 const reverse = (str) => {
-  // write code for strings.reverse
-
+  let splitString = str.split('')
+  let reverseString = splitString.reverse()
+  return reverseString.join('');
 }
 
 module.exports = {
