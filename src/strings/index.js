586d0f006686e69dbf7985039d56c4b4cd788a53
unknown <rmi11s@yahoo.com>
2019-08-29 18:20:56 -0500

    final

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,20 +1,26 @@
 const split = (str, delim) => {
-  // write code for strings.split
+	// write code for strings.split
+	let test = str.split(delim);
+	return test;
+};
 
-}
+const pairs = str => {
+	// write code for strings.pairs
+	let test = str.match(/(..?)/g);
+	return test;
+};
 
-const pairs = (str) => {
-  // write code for strings.pairs
-
-}
-
-const reverse = (str) => {
-  // write code for strings.reverse
-
-}
+const reverse = str => {
+	// write code for strings.reverse
+	let test = str
+		.split("")
+		.reverse()
+		.join("");
+	return test;
+};
 
 module.exports = {
-  split,
-  pairs,
-  reverse
-}
+	split,
+	pairs,
+	reverse
+};
