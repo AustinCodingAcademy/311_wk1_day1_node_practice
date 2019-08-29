17b4470bb123fe10bf8a06bbf976089680257651
Urpiccha <ur.picha.hartman@gmail.com>
2019-08-29 18:35:29 -0500

    First Pull Req

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,20 +1,15 @@
-const split = (str, delim) => {
-  // write code for strings.split
+const split = (str, delim) => {};
 
-}
+const pairs = str => {
+	// write code for strings.pairs
+};
 
-const pairs = (str) => {
-  // write code for strings.pairs
-
-}
-
-const reverse = (str) => {
-  // write code for strings.reverse
-
-}
+const reverse = str => {
+	// write code for strings.reverse
+};
 
 module.exports = {
-  split,
-  pairs,
-  reverse
-}
+	split,
+	pairs,
+	reverse
+};
