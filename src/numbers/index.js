00c0330f54d533c6169e3c230e5440b7e198df59
Alex Munoz <anmunoz24@gmail.com>
2019-10-21 19:00:42 -0700

    ..

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,11 +1,18 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+	if (num % 2 === 0)
+		return true;
+	else
+		return false;
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  let arr = [1,2,3];
+  let sum = arr.reduce(function(a, b) { 
+    return a + b; 
+  }, 0);
+  return sum
 }
 
 const comboSum = (arr, sum) => {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,17 +1,24 @@
 const split = (str, delim) => {
-  // write code for strings.split
+  //write code for strings.split 
+  return str.split(delim)
 
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
+  let arr = str.split("")
 
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let reversedStr = "";    
+  for (let i = str.length - 1; i >= 0; i--){        
+    reversedStr += str[i];
+  }    
+  return reversedStr;
 }
+
 
 module.exports = {
   split,
