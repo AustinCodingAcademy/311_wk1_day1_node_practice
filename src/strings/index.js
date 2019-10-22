9648a5efa664847f50057fc61367a6064f6f3d90
jlmeister <iamphil4483@yahoo.com>
2019-10-21 20:27:27 -0500

    wrote logic for in-class node practice assignment to deal with Dates, numbers, and strings. all tests pass. final commit.

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,22 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim);
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  let arrChars = [];
+  for (let i = 0; i < str.length - 1; i += 2) {
+    arrChars.push(str.slice(i, i + 2));
+  }
+  if (str.length % 2 === 1)
+    arrChars.push(str.slice(str.length - 1));
+  return arrChars;
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  return str.split('').reverse().join('');
 }
 
 module.exports = {
