e488095a88950256f8977e5f02c5799badf36508
kjschwoerke <kjschwoerke@yahoo.com>
2019-10-21 20:46:11 -0500

    first JS311 push

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,18 +1,25 @@
 // import moment here; use this package in each function
+const moment = require('moment');
+moment().format();
+
 
 const today = () => {
   // write code for dates.today
-
+  let date = moment()
+  let day = date.day()
+  return moment.weekdays(day)
 }
 
 const calendar = () => {
   // write code for dates.calendar
-
+  let dateInfo = moment().format('ll')
+  return dateInfo
 }
 
 const currentTime = () => {
   // write code for dates.currentTime
-
+let dateInfo = moment().format('hh:mm:ss A')
+return dateInfo
 }
 
 module.exports = {
