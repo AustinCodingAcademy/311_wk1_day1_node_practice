7cf0daf51fb1433db16dfa1888c1bfe05609894e
Tomson Kan <tomson.kan@gmail.com>
2019-10-23 18:21:11 -0500

    upload

--- src/dates/index.js
+++ src/dates/index.js
@@ -2,17 +2,31 @@
 
 const today = () => {
   // write code for dates.today
+  let d = new Date();
+  let weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
 
+  let n = weekday[d.getDay()];
+  return n;
 }
 
 const calendar = () => {
-  // write code for dates.calendar
+ var moment = require('moment');
+ return moment().format('MMM DD, YYYY');
 
-}
+  }
+
 
 const currentTime = () => {
   // write code for dates.currentTime
-
+  var moment = require('moment');
+  return moment().format("hh:mm:ss A");
 }
 
 module.exports = {
--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,27 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+if( num % 2 == 0) {
+  return true
 }
+}
+
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  return arr.reduce((a, b) => a + b, 0)
 }
+
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  let anySum = arr.reduce((next, current) =>
+     next + current, 0)
+  
+  if(anySum === sum){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
