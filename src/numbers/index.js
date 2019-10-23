4ff36f0572d9cfd61c83480c5bd83b5c049d8956
Connor Yates <connorwyates24@gmail.com>
2019-10-23 18:32:05 -0500

    finished with nodePractice

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,23 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  return num % 2 === 0;
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  return arr.reduce((sum, tot) => {
+    return sum + tot;
+  }, 0);
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  if(arr.indexOf(sum)!== -1){
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 module.exports = {
