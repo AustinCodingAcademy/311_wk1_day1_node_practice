976780522906addd046cea584aa7d0294aae4dbe
Your Name <you@example.com>
2019-10-28 16:55:46 -0500

    first submission

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,14 +1,31 @@
 const isEven = (num) => {
   // write code for numbers.isEven
+  if(num % 2 === 0){
+    return true
+  }
+  }
+isEven(4)
 
-}
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  const sumOfNumbers = arr.reduce((accumulator, currentValue) => {
+return accumulator + currentValue;}, 0);
+  return sumOfNumbers;
 }
+console.log(sum)
+  
 
 const comboSum = (arr, sum) => {
+  
+  for (let i = 0; i<arr.length; i++){
+    const diff = sum - arr[i]
+    if(arr.indexOf(diff) !== -1){
+      return true
+    }
+  }
+      return false
+
   // write code for numbers.comboSum
 
 }
