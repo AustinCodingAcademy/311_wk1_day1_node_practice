1b26fea3b1f826f9d60413f7c9db8b1372657c6b
JustinBarsalou <justin.barsalou@g.austincc.edu>
2019-10-28 17:57:56 -0500

    finished node practice

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,15 +1,29 @@
 const split = (str, delim) => {
   // write code for strings.split
+  let newString = str.split(delim);
+  return newString;
 
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
+  let newString2 = '';
+  let tempString = '';
+  for (i = 0; i < str.length; i+=2){
+    tempString = str.slice(i,i+1);
+    newString2.push(tempString);
+    
+  }
+  return str, newString2
 
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
+  let splitString = str.split('');
+  let arr = splitString.reverse();
+  let newString3 = arr.join('');
+  return newString3;
 
 }
 
