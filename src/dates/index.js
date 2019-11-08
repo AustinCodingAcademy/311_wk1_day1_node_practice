e756b90cfb0c35fbca627d0a416ee46d22419e67
tonezone108 <tonezone108@gmail.com>
2019-11-07 21:41:45 -0600

    Node practice .moment updated

--- src/dates/index.js
+++ src/dates/index.js
@@ -3,52 +3,19 @@ var moment = require("moment");
 moment().format();
 
 const today = () => {
-  // write code for dates.today
-  var d = new Date();
-  var weekday = new Array(7);
-  weekday[0] = "Sunday";
-  weekday[1] = "Monday";
-  weekday[2] = "Tuesday";
-  weekday[3] = "Wednesday";
-  weekday[4] = "Thursday";
-  weekday[5] = "Friday";
-  weekday[6] = "Saturday";
-
-  var n = weekday[d.getDay()];
-  return n;
+  const day = moment().format("dddd");
+  return day;
 };
 
 const calendar = () => {
-  // write code for dates.calendar
-  var d = new Date();
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apl",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec"
-  ];
-  var strDate =
-    months[d.getMonth()] +
-    " " +
-    ("0" + d.getDate()).slice(-2) +
-    ", " +
-    d.getFullYear();
-  console.log(strDate);
-  return strDate;
+  const month = moment().format("MMM DD, YYYY");
+  return month;
 };
 
 // calendar();
 
 const currentTime = () => {
-  time = moment().format("hh:MM:SS A"); //didn't realized I could of used moment, still a good learning experience none the less.
+  const time = moment().format("hh:MM:SS A");
   console.log(time);
   return time;
 };
