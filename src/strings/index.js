d2f8dc79b149298ec5a4242afa28ae15574cd093
Pace Davis <pacemdavis@gmail.com>
2020-01-07 20:56:50 -0600

    working

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,24 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split('-')
 }
 
 const pairs = (str) => {
-  // write code for strings.pairs
+    let newStr= str.split('');
+    let arr = [];
+    // write code for strings.pairs
+   for(let i = 0; i<newStr.length; i += 2) {
+     arr.push((newStr[i] + newStr[i+1]))
+   }
+   return arr
+  }
 
-}
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let newStr = str.split('')
+  let revStr = newStr.reverse()
+  return revStr.join('')
 }
 
 module.exports = {
