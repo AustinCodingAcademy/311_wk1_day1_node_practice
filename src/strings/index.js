3dd9498e3900719b93dea74d616ba312dc27d4ff
rodrieri000 <ericarodriguez7@outlook.com>
2020-01-12 13:17:20 -0600

    Tests passing

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,24 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim);
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  let string= str.split('');
+  let pairs = [];
+  // write code for strings.pairs
+ for (let i = 0; i < string.length; i += 2) {
+  pairs.push(
+    (string[i] + string[i+1])
+  )
+ }
+ return pairs;
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  return str.split("").reverse().join("");
 }
 
 module.exports = {
