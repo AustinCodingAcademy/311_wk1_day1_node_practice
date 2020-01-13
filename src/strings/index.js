ae3bb63c45c433f077d8c55ef87453f2b8bacedd
RiinaMiichelle <sarina.m.colosimo@gmail.com>
2020-01-12 22:08:57 -0600

    finished node practice

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,14 +1,23 @@
 const split = (str, delim) => {
-  // write code for strings.split
-  // let stringSplit = str.split("anewstring", 3);
-  // return stringSplit;
+  // write code for strings.
+  return str.split(delim);
 }
+
 
 
 
 const pairs = (str) => {
   // write code for strings.pairs
+  const results = [];
+  for (let i=0; i < str.length; i = i + 2) {    
+    if (i+1 < str.length) { // do I have two characters left
+      results.push(str.charAt(i) + str.charAt(i + 1));
+    } else {
+      results.push(str.charAt(i));
+    }
+  }
 
+  return results;
 }
 
 const reverse = (str) => {
