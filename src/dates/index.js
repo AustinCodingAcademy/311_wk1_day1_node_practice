464b99b767ac2c2fde651ef0a6eae235167de24e
carl-larson <clarson08@gmail.com>
2020-02-19 15:59:13 -0600

    solved the numbers, dates, and strings practice, all tests pass.

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,18 +1,21 @@
 // import moment here; use this package in each function
+//12120
+const moment = require('moment');
 
 const today = () => {
   // write code for dates.today
-
+  return moment().format('dddd')
 }
 
 const calendar = () => {
   // write code for dates.calendar
-
+  return moment().format('ll')
 }
 
 const currentTime = () => {
   // write code for dates.currentTime
-
+  return moment().format('HH:mm:ss A')
+  // return "01:01:01 AM"
 }
 
 module.exports = {
--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,30 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if (num%2===0) {
+    return true;
+  } else {
+    return false
+  }
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  let total = arr.reduce((prev, current) => prev+current)
+  return total;
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  let isEqual = false;
+  for (let i=0; i<arr.length-1; i++){
+    for (let j=i+1; j<arr.length; j++){
+      let total = arr[i]+arr[j];
+      if (total === sum){
+        isEqual = true;
+      }
+    }
+  }
+  return isEqual;
 }
 
 module.exports = {
