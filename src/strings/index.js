f3249b88f68a9788350ddf7f15bfbfe99986b177
csundbeck <csundbeck04@gmail.com>
2020-02-20 19:33:20 -0600

    Completed week one node practice

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,24 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim);
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  let strLength = str.length;
+  let pairsArr = [];
+  for (let i=0; i<strLength; i=i+2) {
+    let newElem = str[i] + str[i+1];
+    pairsArr.push(newElem);
+  }
+  return pairsArr;
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let strArray = str.split('');
+  strArray.reverse();
+  return strArray.join('');
 }
 
 module.exports = {
