104e6ba4e3cc73bcddc3f407400a9095f308cacf
Magen Crawford <crawford.magen@gmail.com>
2020-02-24 22:19:15 -0600

    day1

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,22 +1,23 @@
 // import moment here; use this package in each function
+const moment = require("moment");
 
 const today = () => {
   // write code for dates.today
-
-}
+  return moment().format("dddd");
+};
 
 const calendar = () => {
   // write code for dates.calendar
-
-}
+  return moment().format("ll");
+};
 
 const currentTime = () => {
   // write code for dates.currentTime
-
-}
+  return moment().format("HH:mm:ss A");
+};
 
 module.exports = {
   today,
   calendar,
   currentTime
-}
+};
