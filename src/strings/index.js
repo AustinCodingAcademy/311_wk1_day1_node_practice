c49b535aebaa72caa2dca4f82e40ef631bed6f90
JillMOry <oryjill@gmail.com>
2020-03-10 22:15:36 -0500

    Day1 Node Practice

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,20 +1,27 @@
 const split = (str, delim) => {
-  // write code for strings.split
-
-}
+	const specSplit = str.split("-");
+	return specSplit;
+};
 
 const pairs = (str) => {
-  // write code for strings.pairs
-
-}
+	// ['ab', 'cd', 'ef', 'gh', 'ij', 'kl', 'mn', 'op']
+	// write code for strings.pairs
+	// honestly I don't have a clue how I did this
+	const split = str.match(/.{1,2}/g);
+	return split;
+};
 
 const reverse = (str) => {
-  // write code for strings.reverse
-
-}
+	// write code for strings.reverse
+	const reVerse = str
+		.split("")
+		.reverse()
+		.join("");
+	return reVerse;
+};
 
 module.exports = {
-  split,
-  pairs,
-  reverse
-}
+	split,
+	pairs,
+	reverse
+};
