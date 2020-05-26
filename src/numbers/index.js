aec8aa81d5ee50d03a7e5b4e55befc96adb01b6a
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-05-26 00:23:13 -0500

    3/3 dates, 2/3 numbers

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,11 +1,13 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if (num % 2 === 0) {
+    return true
+  }
 }
 
 const sum = (arr) => {
-  // write code for numbers.sum
-
+  let reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return arr.reduce(reducer)
 }
 
 const comboSum = (arr, sum) => {
