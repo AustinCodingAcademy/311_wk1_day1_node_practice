55e464daa4f645a39c5c320aabc9f16b4aab74b8
Sean <contact@seanyager.com>
2020-05-27 15:34:25 -0500

    refactored the comboSum

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,23 +1,28 @@
 const isEven = (num) => {
-  // write code for numbers.isEven
+  //if provided num is even return true
   if (num % 2 === 0) return true
+  //otherwise return false
   else return false
 }
 
 const sum = (arr) => {
-  // write code for numbers.sum
+  //establish reducer function
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  //return the result of running the reducer method on the passed array
   return arr.reduce(reducer);
 }
 
 const comboSum = (arr, sum) => {
-  // write code for numbers.comboSum
+  //begin for loop
   for (let i = 0; i <= arr.length; i++){
+    //set the target value to current index minus the sum we are looking for
     let target = arr[i]-sum;
+    //look for the target var value in the arr
     let targetPlace = arr.indexOf(target)
-    if(targetPlace === -1) return false
-    else if (targetPlace != -1) return true
+    //if the target number exists return true
+    if (targetPlace != -1) return true
   }
+  return false;
 }
 
 module.exports = {
