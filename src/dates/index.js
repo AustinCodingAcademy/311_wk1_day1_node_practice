81844b87b498e66fe54ea6d6dc783a26131b42fd
aaron-kane-w <aaron.kane.w@gmail.com>
2020-05-27 19:43:20 -0500

    working on assignment

--- src/dates/index.js
+++ src/dates/index.js
@@ -2,12 +2,20 @@
 
 const today = () => {
   // write code for dates.today
-
+  let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  return days[new Date().getDay()]
 }
 
 const calendar = () => {
   // write code for dates.calendar
+  let today = new Date()
+  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
+  let month = months[today.getMonth()]
+  let day = today.getDate()
+  let year = today.getFullYear()
+
+  return `${month} ${day}, ${year}`
 }
 
 const currentTime = () => {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,22 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim)
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
+  let arr = str.split('')
+  let groups = [];
 
+  for(let i = 0; i < arr.length; i+=2) {
+    groups.push(arr.slice(i, i + 2).join(''))
+  }
+  return groups
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  return str.split('').reverse().join('')
 }
 
 module.exports = {
