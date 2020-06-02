6cdc9005539063db5695bf3991b31830d39dc427
patfitz95 <patfitz95@gmail.com>
2020-06-02 14:36:08 -0500

    finished node practice

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,18 +1,19 @@
 // import moment here; use this package in each function
-
+const moment = require ("moment")
 const today = () => {
   // write code for dates.today
+  return moment().format("dddd")
 
 }
 
 const calendar = () => {
   // write code for dates.calendar
-
+  return moment().format("MMM DD, YYYY")
 }
 
 const currentTime = () => {
   // write code for dates.currentTime
-
+  return moment().format("hh:mm:ss A")
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,22 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim)
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  let strList = []
+  for(let i= 0; i < str.length-1; i++) {
+    if(i%2 ===0) {
+    strList.push(`${str[i]}${str[i+1]}`)
+  }
+  }
+  return strList
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  return str.split("").reverse().join("")
 }
 
 module.exports = {
