7f22a7e255d6dda9c01196544e02e297c2ac240f
Jacoblynch99 <jacoblynch99@gmail.com>
2020-08-10 16:14:32 -0500

    finsished

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,25 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim)
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+const strPair = []
+  for (let i=0; i < str.length; i++) {
+    strPair.push(str[i] + str[i+1])
+    i++
+  }
+  return strPair
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  if (str === ""){
+    return "";
+  } else {
+    return reverse(str.substr(1)) + str.charAt(0);
+  }
 }
 
 module.exports = {
