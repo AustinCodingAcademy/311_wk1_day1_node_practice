a3ca3d573088c594677a7783d7c6b2179f7297c5
ArielHolman <ArielARodrigues01@gmail.com>
2020-08-10 17:14:02 -0500

    created all functions and passing all tests

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,18 +1,18 @@
-// import moment here; use this package in each function
+const moment = require('moment')
 
 const today = () => {
-  // write code for dates.today
-
+  thisday = moment().format("dddd")
+  return thisday
 }
 
 const calendar = () => {
-  // write code for dates.calendar
-
+  thisdate = moment().format('ll')
+  return thisdate
 }
 
 const currentTime = () => {
-  // write code for dates.currentTime
-
+  thistime = moment().format('hh:mm:ss A');
+  return thistime
 }
 
 module.exports = {
