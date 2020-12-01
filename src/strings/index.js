5976d641dd8764be1dd04d771bac838b738b4264
bobbyg211 <robertdg96@gmail.com>
2020-11-30 20:37:12 -0600

    Numebrs & Strings complete

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,20 +1,27 @@
 const split = (str, delim) => {
   // write code for strings.split
-
-}
+  return str.split(delim);
+};
 
 const pairs = (str) => {
   // write code for strings.pairs
-
-}
+  let arr = [];
+  for (let i = 0; i < str.length; i += 2) {
+    arr.push(str.substring(i, i + 2));
+  }
+  return arr;
+};
 
 const reverse = (str) => {
   // write code for strings.reverse
-
-}
+  let arr = str.split("");
+  arr.reverse();
+  let revStr = arr.join("");
+  return revStr;
+};
 
 module.exports = {
   split,
   pairs,
-  reverse
-}
+  reverse,
+};
