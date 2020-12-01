864381dd32b66908771316d7249732fb58c9af8a
ClaireSheek <clairesheek@gmail.com>
2020-12-01 00:17:57 -0600

    worked on numbers and strings

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,8 +1,34 @@
 // import moment here; use this package in each function
+var moment = require('moment'); // require
+moment().format(); 
 
 const today = () => {
   // write code for dates.today
+  let dayOfWeek = moment().day()
 
+ if(dayOfWeek == 0){
+   dayOfWeek = "Sunday";
+ } 
+  else if(dayOfWeek == 1){
+  dayOfWeek = "Monday"
+ }
+ else if(dayOfWeek == 2){
+  dayOfWeek = "Tuesday"
+ }
+  else if(dayOfWeek == 3){
+  dayOfWeek = "Wednesday"
+ }
+  else if(dayOfWeek == 4){
+  dayOfWeek = "Thursday"
+ }
+  else if(dayOfWeek == 5){
+  dayOfWeek = "Friday"
+ }
+  else if(dayOfWeek == 6){
+  dayOfWeek = "Friday"
+ }
+ 
+ console.log(dayOfWeek)
 }
 
 const calendar = () => {
@@ -12,6 +38,7 @@ const calendar = () => {
 
 const currentTime = () => {
   // write code for dates.currentTime
+  
 
 }
 
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,17 +1,30 @@
+const { strings } = require("..")
+
 const split = (str, delim) => {
   // write code for strings.split
+  let stringArray = str.split(delim) //uses the .split method to split the string at the specified index
+  return stringArray //returns the result
 
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
+  let pairsArray = [] //creates an empty array to push the results to
+  for (let i = 0; i < str.length; i+=2) {  //iterates through the string in 2's
+    pairsArray.push(str.substring(i, i+2)) //creates a new substring of two characters and pushes them to the new array
+  }
+ return pairsArray //returns the result
 
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let arrayOfChars = str.split('') //spilts the string into an array of individual characters
+  let reversedArray = arrayOfChars.reverse() //reverses the array of characters
+  let resultString = reversedArray.join("") //converts the array back into a string.
+  return resultString //returns the result
 }
+
 
 module.exports = {
   split,
