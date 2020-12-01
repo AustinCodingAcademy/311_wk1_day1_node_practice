0ccb149842d95080605eb2781bb4114b9650acbc
cartermr <carter.matthew@me.com>
2020-11-30 22:14:34 -0500

    update

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,31 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if (num % 2 == 0) {
+    return true
+  }
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  return arr.reduce((accumulator, currentValue) => accumulator + currentValue )
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  let currentNum = 0
+  for (let i = 0; i < arr.length; i++) {
+    currentNum = currentNum + arr[i]
+    if (currentNum == sum) {
+      return true
+    } else {
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[i] + arr[j] == 14) {
+          return true
+        }
+      }
+    }
+  }
+  return false
 }
 
 module.exports = {
