cb8a59067c637978aa9416a3a91f18b2ed457d7b
@willmcclure <willbmcclure@gmail.com>
2020-11-30 22:04:44 -0600

    Homework partially finished

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,18 +1,31 @@
 // import moment here; use this package in each function
 
+const moment = require("moment")
+
 const today = () => {
   // write code for dates.today
-
+  let c = new Date();
+  c.get
 }
 
 const calendar = () => {
   // write code for dates.calendar
-
+  let d = new Date();
+  let myMonth =   d.toLocaleString('default', { month: 'long' });
+  let myDate = d.getDate();
+  let myYear = d.getFullYear();
+  let z = myMonth + " " + myDate +", " + myYear
+  return z
 }
 
 const currentTime = () => {
   // write code for dates.currentTime
-
+  let d = new Date();
+  let h = d.getHours();
+  let m = d.getMinutes();
+  let s = d.getSeconds();
+  let x = h + ":" + m + ":" + s + " PM";
+  return x
 }
 
 module.exports = {
--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,11 +1,14 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  num % 2 === 0
+  return true
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  let x = arr.reduce(reducer);
+  return x;
 }
 
 const comboSum = (arr, sum) => {
