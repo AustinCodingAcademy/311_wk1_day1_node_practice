0cc0dba45fdfbfbfcde50ad455fdaacdc8b497d7
@willmcclure <willbmcclure@gmail.com>
2020-12-02 18:27:31 -0600

    NodePractice completed

--- src/dates/index.js
+++ src/dates/index.js
@@ -4,28 +4,17 @@ const moment = require("moment")
 
 const today = () => {
   // write code for dates.today
-  let c = new Date();
-  c.get
+  return moment().format("dddd");
 }
 
 const calendar = () => {
   // write code for dates.calendar
-  let d = new Date();
-  let myMonth =   d.toLocaleString('default', { month: 'long' });
-  let myDate = d.getDate();
-  let myYear = d.getFullYear();
-  let z = myMonth + " " + myDate +", " + myYear
-  return z
+  return moment().format("MMM D, yyyy")
 }
 
 const currentTime = () => {
   // write code for dates.currentTime
-  let d = new Date();
-  let h = d.getHours();
-  let m = d.getMinutes();
-  let s = d.getSeconds();
-  let x = h + ":" + m + ":" + s + " PM";
-  return x
+  return moment().format("hh:mm:ss A")
 }
 
 module.exports = {
