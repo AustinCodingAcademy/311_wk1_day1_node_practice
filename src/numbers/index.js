d0f78e1f72b8b8025f375e78d0444ee3d7ce04fa
Stanley M Musial <68257825+StanleyMMusial@users.noreply.github.com>
2021-01-26 15:15:46 -0600

    Finished

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,26 @@
 const isEven = (num) => {
-  // write code for numbers.isEven
-
+  if (num%2 == 0)
+    return true;
+  else
+    return false
 }
 
 const sum = (arr) => {
-  // write code for numbers.sum
-
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  const final = arr.reduce(reducer)
+  return final
 }
 
 const comboSum = (arr, sum) => {
-  // write code for numbers.comboSum
-
+  for (i = 0; i < arr.length; i++) {
+    for(let i2 = 0; i2 < arr.length; i2++){
+      if(arr[i] + arr[i2 == sum]){
+        return true
+      } else {
+        return false
+      }
+      }
+    }
 }
 
 module.exports = {
