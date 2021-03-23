fecb8ec2210ac4609ca9685268cc2ce688664f34
BrandonPerry-co <brandonperrymba@gmail.com>
2021-03-22 22:58:31 -0800

    Pushed changes

--- src/strings/index.js
+++ src/strings/index.js
@@ -5,11 +5,18 @@ const split = (str, delim) => {
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  let newArray = []
+  for (let i = 0; i < str.length; i+=2) {
+    temp = (str[i] + str[i+1])
+    newArray.push(temp.split("").join(""))
+  }
+  // newArray.push(str.split("").join(" , "))
+  return newArray
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
+  return str.split("").reverse().join("");
 
 }
 
