0e567a52c64a5cf7b57672c0aeecb4f2ad87254d
RacMcT <raccmct@gmail.com>
2021-05-10 09:35:35 -0500

    code written, check pass unit tests

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,17 +1,34 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if (num % 2 == 0){
+    return true
+  }
 }
+
+
+
+//For Loop Example from Stack Overflow:
+//  var numbers = [10, 20, 30, 40] // sums to 100 **arr
+//   var sum = 0; **makeup variable name
+// for (var i = 0; i < numbers.length; i++) {
+//   sum += numbers[i]
+// }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+let start = 0;
+for (let i = 0; i < arr.length; i++){
+  start += arr[i]
+}
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
-}
+  if (arr == sum){
+    return true
+  } else
+  return false
+} 
 
 module.exports = {
   isEven,
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,17 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  str.split(delim)
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  for(let i = 0; i < str.length; i+2) // the +2 means it will advance the iterator (i) two units in each iteration- so works for pairs
+   return str.split("")
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  return str.split('').reverse().join('') // split the characters/then reverse the order/then join back together
 }
 
 module.exports = {
