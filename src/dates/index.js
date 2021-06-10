b1810bbdc9aeac66427c2733f688b90ff8db9823
Michael Cole <colemichaelj@yahoo.com>
2021-06-09 21:41:30 -0500

    311_wk1_day1_node_practice

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,17 +1,32 @@
 // import moment here; use this package in each function
-
+const moment = require('moment');
 const today = () => {
   // write code for dates.today
-
-}
+  let d = moment().format('dddd');
+  console.log(d)
+  return d
+} 
 
 const calendar = () => {
   // write code for dates.calendar
+  
+  let c = moment().format('May 29, 2019','mm,dd,yyyy');
+
+  console.log(c);
+  return c
+
 
 }
 
+  
+    
+
+
 const currentTime = () => {
   // write code for dates.currentTime
+  const timeComes = new Date('May 24, 2021 00:20:18 GMT+00:00');
+
+console.log(timeComes)
 
 }
 
