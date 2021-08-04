4a94d8b4bb043594eeea43211e1365cfe93eea74
aguevara1 <albguevara12@gmail.com>
2021-08-03 23:23:46 -0500

    finished all tests

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,18 +1,29 @@
 // import moment here; use this package in each function
+const moment = require('moment');
 
 const today = () => {
   // write code for dates.today
-
+  const weekdays = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday'
+  ]
+  var today = new Date();
+    const day=today.getDay();
+    return weekdays[day];
 }
 
 const calendar = () => {
   // write code for dates.calendar
+  return moment().format('MMM DD, YYYY');
 
 }
 
 const currentTime = () => {
   // write code for dates.currentTime
-
+  return moment().format('hh:mm:ss A');
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,24 @@
 const split = (str, delim) => {
   // write code for strings.split
+  let sum=[];
+    sum=str.split(delim);
+     return sum;
 
-}
+
+ }
 
 const pairs = (str) => {
   // write code for strings.pairs
+   let pairs=[];
+    pairs=str.match(/..?/g);
+    return pairs;
 
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  return str.split("").reverse().join("");
+  
 }
 
 module.exports = {
