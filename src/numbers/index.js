cc2680c9b4246f057bac87873ac42010f8410dc0
Jmirez2022 <Jazi.2000@hotmail.com>
2023-01-08 23:46:31 -0600

    finished

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,19 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  return num % 2 == 0;
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+const result = arr.reduce((a, b) => {
+  return a + b;
+})
+return result;
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  return arr === sum;
 }
 
 module.exports = {
